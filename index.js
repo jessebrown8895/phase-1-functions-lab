2d994f3130fc6315561f2936968c163cd41ac650
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-22 18:22:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(x){
+    if (x >= 42){
+        return x - 42
+    }
+    else{
+        return 42 - x
+    }
+
+}
+
+function distanceFromHqInFeet(y){
+    console.log(distanceFromHqInFeet(y))
+}

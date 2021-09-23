67b389350f57a3b224902848b46a89ce663745b7
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-22 19:57:16 -0500

    Functions Lab
    created a function that returns the distance from Scubers HQ in blocks from the customers pick up location. Then created a function that used the first function as a call back to find the distance that would be travelled in feet. The third function finds the the distance in feet from to random points in a city. the fourth function calculates the fare price. ever foot travelled is 2 cents. However, in the fourth function the first 400 feet are free.

--- index.js
+++ index.js
@@ -10,5 +10,27 @@ function distanceFromHqInBlocks(x){
 }
 
 function distanceFromHqInFeet(y){
-    console.log(distanceFromHqInBlocks(y))
+    return distanceFromHqInBlocks(y) * 264
+}
+
+function distanceTravelledInFeet(x, y){
+    if (x > y){
+        return (x - y) * 264
+    }else{
+        return (y- x) * 264
+    }
+}
+
+function calculatesFarePrice(x, y){
+    const totalFt = distanceTravelledInFeet(x,y)
+    if(totalFt < 400){
+        return 0
+    }else if(totalFt >= 400 && totalFt <= 2000){
+        return (totalFt - 400) * 0.02
+
+    }else if(totalFt >= 2000 && totalFt <= 2500 ){
+        return 25
+    }else{
+        return "cannot travel that far"
+    }
 }
